d899b4e1bb09556e96ac51a3db5a4f2f0966284c
UHyun-K <konglim2@naver.com>
2021-12-07 20:40:13 +0900

    Login, comparing user password

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,6 +1,6 @@
 import express from "express"; //자바스크립트에서는 모든 파일이 자기만의 세계를 가지기 때문에 익스프레스  따로 임폴트 하지 않으면 사용 할 수 없다.
 //하나의 파일에서 돌아가는 환경 만들어야함 ,모듈 개별적임 
-import { getJoin, postJoin, login } from "../controllers/userController";
+import { getJoin, postJoin, getLogin, postLogin } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 
 const rootRouter = express.Router();  //create global router
@@ -9,7 +9,7 @@ const rootRouter = express.Router();  //create global router
 
 rootRouter.get('/', home); //config globla router
 rootRouter.route('/join').get(getJoin).post(postJoin);
-rootRouter.get('/login', login);
+rootRouter.route('/login').get(getLogin).post(postLogin);
 rootRouter.get('/search', search);
 
 
